849ae6c0396c9277a9e912e94ea5f9d1b6520325
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2023-02-06 20:08:57 -0300

    first bank object created

---
+++ Object-bank-exercize/My bank/My bank/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_bank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name; // as my current understand, 'this' keyword is not strictly necessary
+            this.Balance = initialBalance;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
---
+++ Object-bank-exercize/My bank/My bank/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using My_bank;
+
+var agusAccount = new BankAccount("Agus", 60);
+Console.WriteLine($"Account {agusAccount.Number} was created for {agusAccount.Owner} with {agusAccount.Balance} dollars starting money");
